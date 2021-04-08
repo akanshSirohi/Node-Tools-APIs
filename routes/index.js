57896ce00c9450eee748eb885733e977f2ea3a81
Akansh Sirohi <akanshjaat@gmail.com>
2021-04-08 14:46:53 +0530

    Adding some improvements

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,19 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json({
     "text-ttr": {
-      text1: "Primary text to write",
-      text2: "Secondary text to write",
-      bgColor: "Background color of image (#ffffff)",
-      textColor: "Text color of image (#000000)",
+      info:
+        "Creates illusion image with text written on it that only can be read when you tilt your screen",
+      api: {
+        text1: "Primary text to write",
+        text2: "Secondary text to write",
+        bgColor: "Background color of image (#ffffff)",
+        textColor: "Text color of image (#000000)",
+      },
       example: `${baseurl}/text-ttr?text1=Akansh%20Sirohi&text2=Altered%20Coder&bgColor=%23000&textColor=%23f00`,
     },
     ytdl: {
-      url: "Youtube video url",
+      info: "Youtube video downloader",
+      api: { url: "Youtube video url" },
       example: `${baseurl}/ytdl?url=https://www.youtube.com/watch?v=Az-mGR-CehY`,
     },
   });
